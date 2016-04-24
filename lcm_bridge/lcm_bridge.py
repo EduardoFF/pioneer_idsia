054a83e50398d2055484533204da9bd862172909
EduardoFF <eduardo@idsia.ch>
2016-04-25 00:03:40 +0200

    lcm bridge uses new msg type

--- lcm_bridge/lcm_bridge.py
+++ lcm_bridge/lcm_bridge.py
@@ -1,5 +1,5 @@
 import lcm
-from pose_t import pose_t
+from pos_gps_t import pos_gps_t
 from plan_waypoint_t import plan_waypoint_t
 
 """ create lcm """
@@ -8,31 +8,22 @@ lc = lcm.LCM("udpm://239.255.76.67:7667?ttl=1")
 
 
 """ to send pose, just call this one """
+""" robotid (uint8_t) is a parameter of the node """
 def send_pose(robotid, longitude, latitude, altitude):
-    msg = pose_t()
-    channel = "POSE"
+    msg = pos_gps_t()
+    channel = "POSGPS"
     msg.robotid = robotid
-    msg.position =  [longitude,latitude,altitude]
-    msg.orientation = [0,0,0,0]
+    msg.longitude = longitude
+    msg.latitude = latitude
+    msg.altitude = altitude
     lc.publish(channel, msg.encode())
 
 
 """ handles incoming WAYPOINTS """
 def my_handler(channel, data):
-    msg = flow_list_t.decode(data)
-    print("Received message on channel \"%s\"" % channel)
-    print("   timestamp   = %d" % msg.timestamp)
-    print("   addr    = %s" % str(msg.addr))
-    print("   n    = %d" % msg.n)
-    for i in range(msg.n):
-        print "flow_%d: "%(i),
-        print "addr: %s -> %s "%(msg.flows[i].src_addr, msg.flows[i].dst_addr),
-        print "pkt_cnt %d "%(msg.flows[i].pkt_count),
-        print "byte_cnt %d "%(msg.flows[i].byte_count),
-        print "rate %f "%(msg.flows[i].data_rate),
-        print "last_act %d "%(msg.flows[i].last_activity),
-    print("")
-
+    msg = plan_waypoint_t.decode(data)
+    """ the waypoint is: """
+    print "the waypoint is ", msg.latitude, msg.longitude, msg.altitude
     
 subscription = lc.subscribe("RNPPOS", my_handler)
 
